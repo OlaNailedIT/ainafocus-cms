82a16241e04ef6a29dfc4a5ee0cccbc512e3517c
OlaNailedIT <olaolugidan@gmail.com>
2025-03-18 15:28:50 +0100

    Fix SSL for PostgreSQL

--- config/database.ts
+++ config/database.ts
@@ -5,11 +5,11 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL', ''),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'dpg-cvcmua3tq21c73a14ka0-a.render.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'ainafocus_db'),
-        user: env('DATABASE_USERNAME', 'your_db_user'),
-        password: env('DATABASE_PASSWORD', 'your_db_password'),
+        database: env('DATABASE_NAME', 'ainafocus_database'),
+        user: env('DATABASE_USERNAME', 'superadmin'),
+        password: env('DATABASE_PASSWORD', '3NtXnHi5TeGIrXbUsPd0HI2LxcvkeV3H'),
         ssl: env.bool('DATABASE_SSL', false) ? {
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         } : false, // Ensure this is explicitly false when not needed
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', 'localhost'),
+            host: env('DATABASE_HOST', 'dpg-cvcmua3tq21c73a14ka0-a.render.com'),
             port: env.int('DATABASE_PORT', 5432),
             database: env('DATABASE_NAME', 'ainafocus_database'),
             user: env('DATABASE_USERNAME', 'superadmin'),
-            password: env('DATABASE_PASSWORD', 'Superuser7605'),
+            password: env('DATABASE_PASSWORD', '3NtXnHi5TeGIrXbUsPd0HI2LxcvkeV3H'),
             ssl: env.bool('DATABASE_SSL', false),
         },
     },
